dd3f0e9c042c5cd8ebd922d249e5cdefe5952418
thanhlong885 <nguyenthanhlong08085@gmail.com>
2025-09-08 02:45:36 -0400

    refactor and make html code dynamic

--- pages/urls.py
+++ pages/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('cars/', views.cars, name="cars"),
+    path('contact/', views.contact, name="contact"),
+    path('services/', views.services, name="services"),
 ]
